74f5cedc689849ed95b4cfebe6cb550aca2117b8
Osuka-Andala <teresa.andala@gmail.com>
2015-08-19 19:34:54 +0300

    Added next code

--- next.rb
+++ next.rb
@@ -1,3 +1,12 @@
 # Jumps to the next iteration of the loop. 
 # Brings to an end the execution of a block if it is called within a block
 # syntax = next
+
+for i in 0..5
+
+	if i > 3 then
+		break
+	end
+
+	puts "Value of local variable is #{i}"
+end
