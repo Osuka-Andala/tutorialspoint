789108c3b57837f81a3dcf148943facba989fe56
Osuka-Andala <teresa.andala@gmail.com>
2015-08-19 19:27:39 +0300

    Edited the explanation

--- break.rb
+++ break.rb
@@ -1,11 +1,11 @@
 # brings to and end a method inside a block 
-# if the method has been called inside the block
+# if it is called inside the block
 
 # syntax = break
 
 
 for i in 0..5
-	
+
 	if i > 3 then
 		break
 	end
