bbbbab7cad56290430bfdade2ab929c7b3531f19
Osuka-Andala <teresa.andala@gmail.com>
2015-08-19 18:28:44 +0300

    added code

--- whileloop.rb
+++ whileloop.rb
@@ -1,4 +1,14 @@
-# syntax= while condition
+# syntax= while condition 
 	
 # end
 # executes code while condition is true.
+
+
+i = 0
+num = 5
+
+while i < num do
+	puts ("Inside the loop i = #{i}")
+	i += 1
+	
+end
