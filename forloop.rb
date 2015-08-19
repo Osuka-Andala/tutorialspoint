72f1309319f2a0851b0b80cc2681e9049b8dc7c4
Osuka-Andala <teresa.andala@gmail.com>
2015-08-19 19:15:23 +0300

    added .each code

--- forloop.rb
+++ forloop.rb
@@ -4,10 +4,14 @@
 #          end
 # two dots(..) includes the last digit
 
-for i in 0..5
-   puts "Value of local variable is #{i}"
-end
+# for i in 0..5
+#    puts "Value of local variable is #{i}"
+# end
 
 
 # almost the same as
 # (expression).each do |variable[, variable...]| code end
+
+(0..5).each do |i|
+	puts "Value of local variable is #{i}"
+end
