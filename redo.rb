80c647db8f4fce370c3a54ee99c6649dc4622236
Osuka-Andala <teresa.andala@gmail.com>
2015-08-19 19:46:08 +0300

    Added redo code

--- redo.rb
+++ redo.rb
@@ -1,2 +1,9 @@
 # It restarts iteration without checking for the loop conditions
 # syntax = redo
+
+for i in 0..5
+   if i < 2 then
+      puts "Value of local variable is #{i}"
+      redo
+   end
+end
